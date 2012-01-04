b938cd0ad317f7795f889e6149790fdfc3a8a0b1
griff <pochtolyon@gmail.com>
2012-01-05 01:25:38 +0200

    edit models.py, .gitignor

--- blog/models.py
+++ blog/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 class Section(models.Model):
   title = models.CharField(max_length=50)
-  slug = models.SlugField(unique=True, max_length=500)
+  slug = models.SlugField(unique=True, max_length=50)
   
 class Post(models.Model):
   title = models.CharField(max_length=50)
   slug = models.SlugField(unique=True, max_length=50)
-  descrip = models.CharField(max_length=100)
+  descr = models.TextField(max_length=100)
   section = models.ForeignKey(Section)
-  blog = models.TextField(max_length=10000)
+  blog = models.TextField(max_length=100000)
   #tags = TagField()
   creation_date = models.DateTimeField(auto_now_add=True)
-  published_date = models.DateTimeField(auto_now=True)
-  author = models.CharField(max_length=50) #?
+  published_date = models.DateTimeField(null=True, blank=True)
+  author = models.ForeignKey(User, null=True, blank=True, verbose_name=u"Автор")
