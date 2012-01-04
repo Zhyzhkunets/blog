5a73e00b1a57f7ae94ac5e506241ae793accaaf7
griff <pochtolyon@gmail.com>
2012-01-04 17:54:34 +0200

    edit models.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Section(models.Model):
+  title = models.CharField(max_length=50)
+  slug = models.SlugField(unique=True, max_length=500)
+  
+class Post(models.Model):
+  title = models.CharField(max_length=50)
+  slug = models.SlugField(unique=True, max_length=50)
+  descrip = models.CharField(max_length=100)
+  section = models.ForeignKey(Section)
+  blog = models.TextField(max_length=10000)
+  tags = TagField()
+  creation_date = models.DateTimeField(auto_now_add=True)
+  published_date = models.DateTimeField(auto_now=True)
+  #author =    
