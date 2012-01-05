4baf0366383f5341c335a88154932c6140174f18
griff <pochtolyon@gmail.com>
2012-01-05 20:11:40 +0200

    add URL's. The next release

--- urls.py
+++ urls.py
@@ -4,4 +4,13 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
+    (r'^section/$', section_views),
+    (r'^post/$', post_views),
+    (r'^post/list_of_posts_(?P<number>\d+)/$', post_views),
+    (r'^archive/(\d{4})/$', archive_years),
+    (r'^archive/(\d{4})/(\d{2})/$', archive_months),
+    (r'^archive/(\d{4})/(\d{2})/(\d{2})/$', achive_days),    
 )
+
+
+
