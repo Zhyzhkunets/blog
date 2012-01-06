91093946f9b0a63cd6c377f8a40bfe1f5250ebfd
griff <pochtolyon@gmail.com>
2012-01-06 18:34:00 +0200

    The next release

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from mysite.blog.models import Post, Section
+
+def home_views(request):
+  posts = Post.objects.order_by('-id')
+  sections = Section.objects.order_by('-id')
+  return render_to_response('post.html', {'posts':pots, 'sections':sections})
+  
+def section_views(request, name):
+  posts = Post.objects.filter(section=name).order_by('-id')
+  sections = Section.objects.order_by('-id')
+  return render_to_response('post.html', {'posts':pots, 'sections':sections})
+               
+def post_views(request, num="1"):
+  posts = Post.objects.filter(id=num)
+  sections = Section.objects.order_by('-id')
+  return render_to_response('post.html', {'posts':pots, 'sections':sections})
--- urls.py
+++ urls.py
@@ -3,14 +3,14 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^admin/', include(admin.site.urls)),
-    #(r'^section/$', section_views),
-    #(r'^section/(?P<name>w+)/$', section_views),
-    #(r'^post/$', post_views),
-    #(r'^post/(?P<number>\d+)/$', post_views),
-    #(r'^archive/(\d{4})/$', years_views),
-    #(r'^archive/(\d{4})/(\d{2})/$', months_views),
-    #(r'^archive/(\d{4})/(\d{2})/(\d{2})/$', days_views),    
+    #(r'^admin/', include(admin.site.urls)),
+    #(r'^section/$', 'section_views'),
+    (r'^section/(?P<name>w+)/$', 'section_views'),
+    (r'^$', 'home_views'),
+    (r'^post/(?P<num>\d+)/$', 'post_views'),
+    (r'^(\d{4})/(?P<slug>[-w]+)/$', 'years_views'),
+    (r'^(\d{4})/(\d{2})/(?P<slug>[-w]+)/$', 'months_views'),
+    (r'^(\d{4})/(\d{2})/(\d{2})/(?P<slug>[-w]+)/$', 'days_views'),    
 )
 
 
