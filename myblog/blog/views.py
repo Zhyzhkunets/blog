cc03874fe92025555a168e4fffb41e8a9c8d2122
griff <pochtolyon@gmail.com>
2012-01-09 15:53:31 +0200

    add order_by in views.py

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -3,12 +3,13 @@ from models import *
 import datetime
 
 def home_views(request):
-  posts = Post.objects.all().order_by('-id')
+  posts = Post.objects.all().order_by('-published_date')
   return render_to_response('home.html', {'posts':posts})
   
 def section_view(request, slug):
   #section = Section.objects.get(slug=slug)
-  posts = Post.objects.filter(section=Section.objects.get(slug=slug))
+  #posts = Post.objects.filter(section=section)
+  posts = Post.objects.filter(section=Section.objects.get(slug=slug)).order_by('-published_date')
   return render_to_response('section_view.html', {'posts':posts})   
 
 def detail_views(request, year, month, day, slug):
@@ -16,14 +17,14 @@ def detail_views(request, year, month, day, slug):
   return render_to_response('detail_views.html', {'post':post})
 
 def years_views(request, year):
-  posts = Post.objects.filter(creation_date__year=year)
+  posts = Post.objects.filter(creation_date__year=year).order_by('-published_date')
   return render_to_response('years_views.html', {'posts':posts})
 
 def months_views(request, year, month):
-  posts = Post.objects.filter(creation_date__year=year, creation_date__month=month)
+  posts = Post.objects.filter(creation_date__year=year, creation_date__month=month).order_by('-published_date')
   return render_to_response('months_views.html', {'posts':posts})
 
 def days_views(request, year, month, day):
-  posts = Post.objects.filter(creation_date__year=year, creation_date__month=month, creation_date__day=day)
+  posts = Post.objects.filter(creation_date__year=year, creation_date__month=month, creation_date__day=day).order_by('-published_date')
   return render_to_response('days_views.html', {'posts':posts})
    
