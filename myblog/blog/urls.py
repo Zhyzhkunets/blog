c2f708494db68dfbf335ebec50ef54780fc654f4
griff <pochtolyon@gmail.com>
2012-01-17 17:49:14 +0200

    generic views

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -1,35 +1,36 @@
 from django.conf.urls.defaults import *
-from django.views.generic import list_detail
+from django.views.generic import date_based
 from views import *  
+from myblog.blog.models import *
 
 years_info = {
-  'queryset':Post.object.all(),
+  'queryset':Post.objects.all(),
   'date_field':'creation_date', 
-  'template_name ':'templates/archive_years.html', 
-  'make_object_list'=True, 
+  'template_name ':'templates/archive_year.html',  
+  'allow_future':True,
 }
 
- months_info = { 
-  'queryset':Post.object.all(), 
+months_info = { 
+  'queryset':Post.objects.all(), 
   'date_field':'creation_date',   
-  'template_name':'templates/archive_months.html',   
+  'template_name':'templates/archive_month.html',
+  'allow_future':True,
 }                                 
 
-posts_info = {
-  'queryset':Post.object.all(),
+days_info = {
+  'queryset':Post.objects.all(),
   'date_field':'creation_date',  
-  'template_name':'templates/archive_days.html',    
-                    }                                 
-#detail_views = {}                                 
+  'template_name':'templates/archive_day.html',    
+  'allow_future':True,
+  'template_object_name':True,
+}                                 
 
 urlpatterns = patterns('',
   url(r'^$', home_views),
   url(r'^(?P<slug>[-\w]+)/$', section_view),
   url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[-\w]+)/$', detail_views, name="post_url"),
+  url(r'^(?P<year>\d{4})/$', date_based.archive_year, years_info, name="archive_years"),
+  url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', date_based.archive_month, months_info, name="archive_months"),
+  url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$', date_based.archive_day, days_info, name="archive_days"),
 )  
-                          
-urlpatterns +=patterns('django.views.generic.date_based' 
-  url(r'^(?P<year>\d{4})/$','archive_month', month_info),
-  url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', 'archive_day', day_info),  
-  url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', 'object_detail', posts_info),
                                 
