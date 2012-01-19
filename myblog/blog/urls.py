753fdcacc96c5f9f340ab6218bea3b9a67ea9a0f
griff <pochtolyon@gmail.com>
2012-01-19 19:01:55 +0200

    archive_year work, archive_month archive_day doesn't work

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -6,28 +6,28 @@ from myblog.blog.models import Post
 years_info = {
   'queryset':Post.objects.all(),
   'date_field':'creation_date',
-  'template_name ':'templates/archive_year.html',  
+  'template_name':'archive_year.html',  
 }
 
 months_info = { 
   'queryset':Post.objects.all(), 
   'date_field':'creation_date',
-  'template_name':'templates/archive_month.html',
+  'template_name':'archive_month.html',
 }                                 
 
 days_info = {
   'queryset':Post.objects.all(),
   'date_field':'creation_date', 
-  'template_name':'templates/archive_day.html',    
-  'make_object_list':True,
+  'template_name':'archive_day.html',    
+  #'make_object_list':True,
 }                                 
 
 urlpatterns = patterns('',
   url(r'^$', home_views),
-  url(r'^(?P<slug>[-\w]+)/$', section_view),
-  url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[-\w]+)/$', detail_views, name="post_url"),
   url(r'^(?P<year>\d{4})/$', date_based.archive_year, years_info, name="archive_years"),
-  url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', date_based.archive_month, months_info, name="archive_months"),
+  url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$', date_based.archive_month, months_info, name="archive_months"),
   url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$', date_based.archive_day, days_info, name="archive_days"),
+  url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[-\w]+)/$', detail_views, name="post_url"),      
+  #url(r'^(?P<slug>[-\w]+)/$', section_view),
 )  
                                 
