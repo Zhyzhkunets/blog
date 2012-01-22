1e7950fc0c1854ee2bb767e40b9627e868da4bb2
griff <pochtolyon@gmail.com>
2012-01-22 15:52:32 +0200

    urls.py ->detail_info no working

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls.defaults import *
-from django.views.generic import date_based
+from django.views.generic import date_based, list_detail
 from myblog.blog.models import Post
 from time import strftime
 from views import *  
 import time
 
+home_info = {
+  'queryset':Post.objects.all(),
+  'template_name':'home.html',
+}
 
 years_info = {
   'queryset':Post.objects.all(),
@@ -26,12 +30,22 @@ days_info = {
   'month_format':time.strftime('%m'),
 }                                 
 
+detail_info = {
+  'queryset':Post.objects.all(),
+  'date_field':'creation_date',
+  'slug_field':'slug',
+  'month_format':time.strftime('%m'),
+  'template_name':'detail_views.html',
+  'template_object_name':'item',
+  'extra_context':{'post':Post.objects.all()},
+}
+
 urlpatterns = patterns('',
-  url(r'^$', home_views),
+  url(r'^$', list_detail.object_list, home_info),
   url(r'^(?P<year>\d{4})/$', date_based.archive_year, years_info, name="archive_years"),
-  url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$', date_based.archive_month, months_info, name="archive_months"),
-  url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$', date_based.archive_day, days_info, name="archive_days"),
-  url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[-\w]+)/$', detail_views, name="post_url"),      
+  url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', date_based.archive_month, months_info, name="archive_months"),
+  url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', date_based.archive_day, days_info, name="archive_days"),
+  url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', date_based.object_detail, detail_info, name="post_url"),      
   #url(r'^(?P<slug>[-\w]+)/$', section_view),
 )  
                                 
